0dae124fa03ed593a499e1951f797c3f9d726452
soyoung125 <sypark041754@gmail.com>
2024-07-31 17:27:23 +0900

    feat: post 등록

--- week3/day13/src/app/(blog)/write/page.tsx
+++ week3/day13/src/app/(blog)/write/page.tsx
@@ -1,4 +1,65 @@
+"use client";
+
+import { TPost } from "@/types/post";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 export default function WritePAge() {
+  const router = useRouter;
+  const [text, setText] = useState<TPost>({
+    id: uuidv4(),
+    title: "",
+    category: "",
+    description: "",
+    thumbnail: "",
+    datetime: new Date(),
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
+    setText({
+      ...text,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0]; // 파일을 가져온다.
+    if (file) {
+      const reader = new FileReader(); // 파일을 읽어오는 객체를 생성한다.
+      reader.onloadend = () => {
+        // 파일을 다 읽었을 때 실행되는 이벤트
+        setText({
+          ...text,
+          thumbnail: reader.result as string, // 파일을 base64로 변환한 값을 저장한다.
+        });
+      };
+      reader.readAsDataURL(file); // 파일을 읽어온다.
+    }
+  };
+
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST_URL}/api/blog`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(text),
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
       <section className="bg-white ">
@@ -6,22 +67,23 @@ export default function WritePAge() {
           <h2 className="mb-4 text-xl font-bold text-gray-900 ">
             Add a new Post
           </h2>
-          <form action="#">
+          <form onSubmit={onSubmitHandler}>
             <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
               <div className="sm:col-span-2">
                 <label
-                  htmlFor="name"
+                  htmlFor="title"
                   className="block mb-2 text-sm font-medium text-gray-900 "
                 >
                   Title
                 </label>
                 <input
                   type="text"
-                  name="name"
-                  id="name"
+                  name="title"
+                  id="title"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="Type product name"
                   required
+                  onChange={handleChange}
                 />
               </div>
               <div>
@@ -33,7 +95,9 @@ export default function WritePAge() {
                 </label>
                 <select
                   id="category"
+                  name="category"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                  onChange={handleChange}
                 >
                   <option value={""}>Select category</option>
                   <option value="Next.js">Next.js</option>
@@ -51,15 +115,17 @@ export default function WritePAge() {
                 </label>
                 <label
                   className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  htmlFor="user_avatar"
+                  htmlFor="thumbnail"
                 >
                   Upload file
                 </label>
                 <input
                   className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50focus:outline-none   h-[42px] p-[7px]"
-                  aria-describedby="user_avatar_help"
-                  id="user_avatar"
+                  aria-describedby="thumbnail_help"
+                  id="thumbnail"
+                  name="tumbnail"
                   type="file"
+                  onChange={handleImageUpload}
                 />
               </div>
               <div className="sm:col-span-2">
@@ -71,9 +137,11 @@ export default function WritePAge() {
                 </label>
                 <textarea
                   id="description"
+                  name="description"
                   rows={8}
                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
                   placeholder="Your description here"
+                  onChange={handleChange}
                 ></textarea>
               </div>
             </div>
---
+++ week3/day13/src/app/api/blog/route.ts
@@ -0,0 +1,28 @@
+// http://localhost:3000/api/blog (POST)
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  // store at db
+  try {
+    // db에 저장하는 로직
+    const res = await fetch(`http://localhost:5500/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    if (res.ok)
+      return Response.json({ message: "등록에 성공했습니다.", status: true });
+    else
+      return Response.json({ message: "등록에 실패했습니다.", status: false });
+  } catch (e) {
+    if (e instanceof Error) {
+      return Response.json({ message: e.message, status: false });
+    }
+    return Response.json({
+      message: "알 수 없는 오류가 발생했습니다.",
+      status: false,
+    });
+  }
+}
