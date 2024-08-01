e6e2ed9359872fbd03e3b8abdf9327cbb25759f5
soyoung125 <sypark041754@gmail.com>
2024-08-01 13:57:47 +0900

    feat: Server Action으로 데이터 저장하기

--- week3/day13/src/app/(ex)/mongo/page.tsx
+++ week3/day13/src/app/(ex)/mongo/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+import { addProduct } from "@/server-action/product.action";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
+
 export default function MongoPage() {
+  const [state, formAction] = useFormState(addProduct, {
+    message: "",
+    status: true,
+  }); // 클라이언트에서만 사용 가능
+  //addProduct 를  useFormState로 래핑한 것임.
+
+  useEffect(() => {
+    if (state.message !== "") {
+      alert(state.message);
+    }
+  }, [state]);
+
   return (
     <>
       <h1>MongoPage</h1>
+      {JSON.stringify(state)}
+      <form action={formAction}>
+        <input type="text" name="product" />
+        <br />
+        <input type="number" name="price" />
+        <br />
+        <button type="submit">submit</button>
+      </form>
     </>
   );
 }
---
+++ week3/day13/src/server-action/product.action.ts
@@ -0,0 +1,20 @@
+"use server"; // 반드시 붙여줘야 함
+import { connectToDB } from "@/libs/db";
+import { Product } from "@/libs/schema/product";
+
+export async function addProduct(
+  state: {
+    message: string;
+    status: boolean;
+  },
+  formData: FormData
+) {
+  // mongo db
+  connectToDB();
+
+  await Product.create({
+    name: formData.get("product"),
+    price: formData.get("price"),
+  });
+  return { message: "Product added", status: true };
+}
