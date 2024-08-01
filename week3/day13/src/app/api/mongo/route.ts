4cbfb37d70d9c90853c32a48e16cefa7e71b5626
soyoung125 <sypark041754@gmail.com>
2024-08-01 13:57:25 +0900

    feat: mongoDB의 데이터 불러오기

--- week3/day13/src/app/api/mongo/route.ts
+++ week3/day13/src/app/api/mongo/route.ts
@@ -4,7 +4,14 @@ import { Product } from "@/libs/schema/product";
 export async function GET(request: Request) {
   connectToDB(); // db 연결/ 항상 먼저 선언해야 함.
 
-  const res = await Product.create({ name: "Product 1", price: 1000 });
+  const res = await Product.find({});
+  return Response.json(res);
+}
+
+export async function POST(request: Request) {
+  connectToDB(); // db 연결/ 항상 먼저 선언해야 함.
+
+  const res = await Product.create({ name: "Product 2", price: 2000 });
   // 프로덕트에 해당 데이터를 넣겠다.
   return Response.json(res);
 }
